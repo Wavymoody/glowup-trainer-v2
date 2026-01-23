e6e2f60c5ce7ceb01c772d826ffd95a7ab2903ed
Mahmoud <melbayoumi051@gmail.com>
2026-01-22 21:09:40 -0500

    init: spring boot project scaffold

---
+++ src/main/java/com/glowuptrainer/GlowupTrainerV2Application.java
@@ -0,0 +1,13 @@
+package com.glowuptrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowupTrainerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlowupTrainerV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/glowuptrainer/GlowupTrainerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glowuptrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlowupTrainerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
